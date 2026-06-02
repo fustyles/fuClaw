12f5b7da38b4e91ab29a1e2a41104e156ad10a93
fustyles <34397289+fustyles@users.noreply.github.com>
2026-06-03 00:16:01 +0800

    Update AmebaPro2_TelegramBot_fuClaw.ino

--- AmebaPro2/AmebaPro2_TelegramBot_fuClaw/AmebaPro2_TelegramBot_fuClaw.ino
+++ AmebaPro2/AmebaPro2_TelegramBot_fuClaw/AmebaPro2_TelegramBot_fuClaw.ino
@@ -1094,7 +1094,7 @@ void executeTool(String workId, String command, JsonObject params, bool reCheck 
        
       String response =
         "{\"status\":\"success\","
-        "\"method\":\"still\","
+        "\"method\":\"/still\","
         "\"result\":\"" + res + "\"}";
     
       historicalMessages += buildGeminiMessage("user", command + timestamps);
