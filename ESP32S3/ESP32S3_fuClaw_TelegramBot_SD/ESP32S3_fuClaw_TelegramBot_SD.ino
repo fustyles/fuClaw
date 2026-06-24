48aa14df2507aa44bc4f302c5e577593e7c978b9
fustyles <34397289+fustyles@users.noreply.github.com>
2026-06-24 15:50:02 +0800

    Update ESP32S3_fuClaw_TelegramBot_SD.ino

--- ESP32S3/ESP32S3_fuClaw_TelegramBot_SD/ESP32S3_fuClaw_TelegramBot_SD.ino
+++ ESP32S3/ESP32S3_fuClaw_TelegramBot_SD/ESP32S3_fuClaw_TelegramBot_SD.ino
@@ -126,7 +126,7 @@ Supported Tools
 Persistent Files
 ------------------------------------------------------------
 env.json
-  WiFi / Telegram / Gemini credentials / Time zone
+  Device Name / WiFi / Telegram / Gemini credentials / Time zone
 
 device.md
   Devices definition
